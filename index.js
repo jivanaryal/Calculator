49f1274f3c4a77f0ffc4e6047316a7692f7de354
jivanaryal <jeevanaryal7947@gmail.com>
2022-08-03 11:18:28 +0545

    jivan commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let string = "";
+let buttons = document.querySelectorAll('#button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+
+        console.log(e.target);
+    
+       
+    })
+})
+
